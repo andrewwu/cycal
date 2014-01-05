0f21a319c0cd2d812575a74f4f3ebde3b3d61d7b
Andrew Wu <wujuichang@gmail.com>
2014-01-04 16:22:53 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cupcake::Application.config.secret_key_base = '2a5d38161f360a30078564d86845b9510267c40236b85df21042f4b993bbe14b7625c00e3dd1744459cf19b034af0f5a7900923cff5234a18cc98581ed5e2e78'
