87dc03196b90c657e4fc2384231bd0880db08144
Andrew Wu <wujuichang@gmail.com>
2014-02-23 17:39:57 -0800

    Refactor JS and split calendar elements into partials

---
+++ app/assets/javascripts/calendar.js
@@ -0,0 +1,254 @@
+// VARIABLES
+var categoryArray = new Array();
+var eventArray = new Array();
+var categoryIndex = 1;
+var viewModel;
+var calendar;
+
+// CONSTANTS
+var RIDE_KEY = 'events';
+var RIDE_TYPE_KEY = 'categories';
+
+// VIEW MODELS
+function CategoryViewModel(id, label, color) {
+  this.id = id;
+  this.label = ko.observable(label);
+  this.color = ko.observable(color);
+
+  this.editing = ko.observable(false);
+  this.edit = function() { this.editing(true) }
+
+  this.editingColor = ko.observable(false);
+  this.editColor = function() { this.editingColor(true) }
+
+  this.editing.subscribe(function(editing) {
+    if (!editing) {
+      // TODO: validate category label
+      save();
+      refresh();
+    }
+  });
+
+  this.editingColor.subscribe(function(editing) {
+    if (!editing) {
+      // TODO: validate category color
+      save();
+      refresh();
+    }
+  });
+}
+
+function CategoriesViewModel(categories) {
+  var self = this;
+
+  self.categories = ko.observableArray(categories);
+
+  self.addCategory = function() {
+    var label= $('input#label').val();
+    var color = $('input#color').val();
+    var category = new CategoryViewModel(categoryIndex, label, color);
+
+    // remember to always increment after adding; this is the only time to do so
+    categoryIndex += 1;
+    self.categories.push(category);
+    save();
+  }
+
+  self.removeCategory = function(category) {
+    var associatedEvents = findEventsByCategoryId(category.id);
+
+    if (associatedEvents.length > 0) {
+      alert("There are events associated with this category. Please reassign them first.");
+      return;
+    }
+
+    if (confirm('Are you sure you want to delete the category: ' + category.label() + '?')) {
+      self.categories.remove(category);
+      save();
+    }
+  }
+}; 
+
+// ON LOAD
+$(function() {
+  $('.chosen-select').chosen({ width: '100%' });
+
+  calendar = $('.calendar').clndr({
+    daysOfTheWeek: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    template: $('#template-calendar').html(),
+    events: eventArray,
+    weekOffset: 1
+  });
+
+  // TODO: more descriptive binding
+  $('.btn-primary').click(function() {
+    var date = $('#activityForm').data('date');
+
+    // TODO: validation here in another function
+
+    var hours = parseInt($('#hours').val());
+    var minutes = parseInt($('#minutes').val());
+    var category_id = parseInt($('#category').val()); // #TODO: make this an integer
+    var duration = (hours * 60) + minutes;
+
+    var event = {
+      date: date,
+      duration: duration,
+      category_id: category_id
+    }
+
+    if (inEditMode()) {
+      var result = findEventByDate(date);
+      var index = eventArray.indexOf(result);
+      eventArray[index] = event;
+    } else {
+      eventArray.push(event);
+    }
+
+    refresh();
+    save();
+    $('.modal').modal('hide');
+  });
+
+  // TODO: more descriptive binding
+  $('.modal-footer .btn-danger').click(function() {
+    if (confirm('Are you sure?')) {
+      $('.modal').modal('hide');
+
+      var date = $('#activityForm').data('date');
+      var result = findEventByDate(date)
+
+      var index = eventArray.indexOf(result);
+      eventArray.splice(index, 1);
+      refresh();
+      save();
+    }
+  });
+
+  loadFromStorage();
+
+  categoryModels = new Array();
+
+  for (var i = 0; i < categoryArray.length; i++) {
+    category = categoryArray[i];
+    categoryModels.push(new CategoryViewModel(category.id, category.label, category.color));
+  }
+
+  viewModel = new CategoriesViewModel(categoryModels);
+  ko.applyBindings(viewModel);
+  refresh();
+});
+
+// HELPERS
+function setCategoryIndex() {
+  var max = 1;
+
+  for (var i = 0; i < categoryArray.length; i++) {
+    var id = parseInt(categoryArray[i].id)
+    if (id > max) {
+      max = id;
+    }
+  }
+
+  categoryIndex = max + 1;
+}
+
+function findEventByDate(date) {
+  var result = $.grep(eventArray,
+                      function(e) { return e.date == date });      
+  return result[0];
+}
+
+function findEventsByCategoryId(category_id) {
+  var result = $.grep(eventArray,
+                      function(e) { return e.category_id == category_id; });
+  return result;
+}
+
+function findCategoryById(id) {
+  var result = $.grep(viewModel.categories(), 
+                      function(e) { return e.id == id; });
+
+  if (result.length == 0) {
+    return false
+  } else if (result.length == 1) {
+    //found it
+    return result[0];
+  } else {
+    // multiple items found this should never happen since ids are guaranteed to be unique
+  }
+}
+
+function loadFromStorage() {
+  if (!Modernizr.localstorage) { return false; }
+
+  // TODO: change these to constant keys
+
+  if (localStorage[RIDE_KEY] !== undefined && localStorage[RIDE_TYPE_KEY] !== undefined) {
+    eventArray = JSON.parse(localStorage[RIDE_KEY]);
+    categoryArray = JSON.parse(localStorage[RIDE_TYPE_KEY]);
+
+    setCategoryIndex(); 
+  } else {
+    // no previous data
+  }
+}
+
+function save() {
+  if (Modernizr.localstorage) {
+    localStorage[RIDE_KEY] = JSON.stringify(eventArray);
+    localStorage[RIDE_TYPE_KEY] = ko.toJSON(viewModel.categories());
+  } else {
+    // TODO: no localstorage support
+  }
+}
+
+function refresh() {
+  calendar.setEvents(eventArray);
+}
+
+// MODAL
+// TODO: more descriptive binding
+$(document).on('click', '[data-toggle="modal"]', function() {
+  var event = $(this).hasClass('event');
+  $('#activityForm').data('date', $(this).data('date'));
+
+  if (event) {
+    var index = parseInt($(this).data('index'));
+    $('.modal').data('mode', 'edit');
+    setModalMode('edit');
+  } else {
+    setModalMode('new');
+  }
+});
+
+function setModalMode(mode) {
+  $('#activityForm').data('mode', mode);
+
+  if (mode == 'new') {
+    $('.btn-danger').hide();
+    $('#hours').val('');
+    $('#minutes').val('');
+    $('#category').val('0');
+  } else {
+    // edit mode
+    $('.btn-danger').show();
+
+    var date = $('#activityForm').data('date');
+    var result = findEventByDate(date);
+
+    var hours = Math.floor(result.duration / 60);
+    var minutes = result.duration % 60;
+    var category = result.category_id;
+
+    $('#hours').val(hours);
+    $('#minutes').val(minutes);
+    $('#category').val(category);
+  }
+
+  $('#category').trigger('chosen:updated');
+}
+
+function inEditMode() {
+  return $('.modal').data('mode') == 'edit';
+}
